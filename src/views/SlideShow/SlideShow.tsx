3de7e64e10f3d5b4bff347010cf07455f57170f2
jack jephson <jackjephson@jacks-MacBook-Pro.local>
2023-12-05 16:57:26 +0000

    slideshow done with button inbetween

--- src/views/SlideShow/SlideShow.tsx
+++ src/views/SlideShow/SlideShow.tsx
@@ -1,39 +1,56 @@
-import React from 'react';
-import {useEffect, useState} from 'react'
-import Image1 from 'assets/image9.png'
+import React, { useState, useEffect } from 'react';
 import imagearray from 'components/Image/Image';
 
 function SlideShow() {
-    
-    const delay = async (ms: number) => {
-        return new Promise((resolve) => 
-            setTimeout(resolve, ms));
-    };
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const delay = async (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
 
-    useEffect(() => {
-        console.log("Current index is " , currentImageIndex)
-        changeImage()
-    })
-    const changeImage =async () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [showButton, setShowButton] = useState(false);
+  const [startTime, setStartTime] = useState(null);
+  const [responseTime, setResponseTime] = useState(null);
 
-        if (currentImageIndex !== imagearray.length -1 ) {
-            console.log(currentImageIndex)
-            await delay(1000)
-    
-            setCurrentImageIndex(currentImageIndex + 1);
-        }
+  const changeImage = async () => {
+    if (currentImageIndex < imagearray.length - 1) {
+      await delay(1000);
+      setShowButton(true);
+      setStartTime(Date.now()); // Record the start time when the button is shown
+    }
+  };
 
-    };
+  const handleButtonClick = () => {
+    setShowButton(false);
+    const endTime = Date.now();
+    setResponseTime(endTime - startTime); // Calculate the response time
+    setCurrentImageIndex((prevIndex) => prevIndex + 1);
+  };
 
-    return (
-        <>
-        <div>
-            <img src={imagearray[currentImageIndex]}  alt='' style={{ width: '500px', height: '500px', margin: '5px' }} />
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      changeImage();
+    }, 1000);
 
-        </div>
+    return () => clearTimeout(timer);
+  }, [currentImageIndex]);
 
-        </>
-    )
+  return (
+    <div>
+      {showButton ? (
+        <button onClick={handleButtonClick}>Next Slide</button>
+      ) : (
+        <img
+          src={imagearray[currentImageIndex]}
+          alt=""
+          style={{ width: '500px', height: '500px', margin: '5px' }}
+        />
+      )}
+
+      {responseTime !== null && (
+        <p>Response Time: {responseTime} milliseconds</p>
+      )}
+    </div>
+  );
 }
-export default SlideShow
+
+export default SlideShow;
