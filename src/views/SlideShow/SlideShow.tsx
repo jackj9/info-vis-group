f1fd5c1b1906c73d8ff204011d8a15f9ef064f6e
jack jephson <jackjephson@jacks-MacBook-Pro.local>
2023-12-06 12:06:10 +0000

    Quiz boilerplate done with response time but no api requests

--- src/views/SlideShow/SlideShow.tsx
+++ src/views/SlideShow/SlideShow.tsx
@@ -1,82 +1,119 @@
 import React, { useState, useEffect } from 'react';
 import imagearray from 'components/Image/Image';
+import questions from "assets/questions.json"
 
 function SlideShow() {
   const delay = async (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
+
+
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [showButton, setShowButton] = useState(false);
-  const [slideShowStarted, setSlideShowStarted] = useState(false);
+  const [showButton, setShowButton] = useState<boolean>(false);
+  const [slideShowStarted, setSlideShowStarted] = useState<boolean>(false);
+  const [slideShowEnded, setSlideShowEnded] = useState<boolean>(false);
 
   const [startTime, setStartTime] = useState(null);
   const [responseTime, setResponseTime] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState<Question>(null)
+  const [showContent, setShowContent] = useState<boolean>(true)
 
-  const startSlideshow = async () => {
-    setShowButton(false);
-    await delay(1000); // Initial delay before showing the first image
-    setStartTime(Date.now()); // Record the start time when the first image is shown
-    setShowButton(true);
-  };
 
-  const changeImage = async () => {
-    if (currentImageIndex < imagearray.length - 1) {
-      await delay(1000);
-      setStartTime(Date.now()); // Record the start time when the button is shown
-      setShowButton(true);
+  const handleButtonClick = async (choice: string) => {
+
+
+    var endTime = Date.now()
+
+    setResponseTime(endTime - startTime)
+    if (currentImageIndex != questions.length - 1) {
+      setShowContent(false)
+      delay(1000).then(() => {
+        setShowContent(true)
+        setStartTime(Date.now())
+        console.log(choice)
+        setCurrentImageIndex((prevIndex) => prevIndex + 1);
+        setCurrentQuestion(questions[currentImageIndex])
+        
+      })
     }
+
+    else {
+      console.log("Api request")
+      // setSlideShowStarted(false)
+      setShowContent(false)
+
+      setSlideShowEnded(true)
+    }
+  
   };
 
-  const handleButtonClick = () => {
-    setShowButton(false);
-    const endTime = Date.now();
-    setResponseTime(endTime - startTime); // Calculate the response time
-    setCurrentImageIndex((prevIndex) => prevIndex + 1);
-  };
-
-  const slideShowInit = () => {
+  const slideShowInit = async () => {
+    
     setSlideShowStarted(true)
-  };
 
+    setStartTime(Date.now())
+    await delay(1000); // Initial delay before showing the first image
+
+  };
 
 
   useEffect(() => {
-    console.log(slideShowStarted )
-    console.log(currentImageIndex )
-
-    if (currentImageIndex === 0 && slideShowStarted == true) {
-      startSlideshow();
-    }
-    else if (currentImageIndex !== 0 && slideShowStarted == true){
-      changeImage();
-    }
+    setCurrentQuestion(questions[currentImageIndex]);
   }, [currentImageIndex]);
 
+
   return (
-    <div>
-    {slideShowStarted  ? (
+    <>
+      {slideShowStarted ? (
         <>
-              {showButton ? (
-        <button onClick={handleButtonClick}>Next Slide</button>
+          {showContent ? (
+            <>
+              <img
+                src={imagearray[currentImageIndex]}
+                alt=""
+                style={{ width: '500px', height: '500px', margin: '5px' }}
+              />
+
+              <p>{currentQuestion.question}</p>
+              {currentQuestion.choices.map((choice, choiceIndex) => (
+
+                <button key={choiceIndex} onClick={() => handleButtonClick(choice)}>
+                  {choice}
+                </button>
+
+              ))}
+              {responseTime !== null && (
+                <p>Response Time: {responseTime} milliseconds</p>
+              )}
+
+            </>
+          ) : (
+            <></>
+        )}
+
+
+        </>
       ) : (
-        <img
-          src={imagearray[currentImageIndex]}
-          alt=""
-          style={{ width: '500px', height: '500px', margin: '5px' }}
-        />
+        <>
+        <p>This is a test for information visulization . Do not refresh the page at any point during the test or it will restart</p>
+        <button onClick={slideShowInit}>start</button>
+      </>
+      )
+      
+      
+      }
+
+      {slideShowEnded && (
+        <p>This slideshow is over thank you </p>
       )}
-        </>
-    ) : (
-        <>        <button onClick={slideShowInit}>start</button>
-        </>
-    )}
-
     
-
+      
 
  
-    </div>
+
+
+    </>
   );
 }
 
