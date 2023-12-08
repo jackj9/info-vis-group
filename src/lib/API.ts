cc5d4fbd0ac1d2dbb9c7709b9cdad0746b02a186
jack jephson <jackjephson@jacks-MacBook-Pro.local>
2023-12-08 11:22:27 +0000

    api requests done

---
+++ src/lib/API.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+
+const url = "http://172.187.146.140:8080";
+
+
+
+const axiosInstance = axios.create()
+
+
+export default class API {
+  static sendResult(result: NewTrialResult[]): Promise<NewTrialResponse> {
+      
+       return axiosInstance.post<NewTrialResponse>(`${url}/result/new`, result)
+      .then((response: AxiosResponse<NewTrialResponse>) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.error('API request error:', error);
+        throw error; 
+      });
+  }
+}
